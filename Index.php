6bd08e52d0e7736e4373e3cd681e40153c81b6e1
edsson878 <30772340+edsson878@users.noreply.github.com>
2026-08-27 11:18:46 -0600

    v2: clase 2: completado con IA

--- Index.php
+++ Index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="/assets/js/vue/vue.min.js"></script>
 </head>
+
 <body>
     <div id="app">
         <input v-model="message">
@@ -13,10 +15,28 @@
     </div>
 
     <script>
-        var app =new Vue({
+        var app = new Vue({
             el: '#app',
-            data: {
-                message: 'Hello, Vue!'
+            data() {
+                return {
+                    message: 'Hello, Vue!'
+                };
+            },
+            created() {
+                const self = this; // Preserva la referencia a la instancia de Vue
+                const xhr = new XMLHttpRequest();
+                xhr.open('GET', 'prueba.php', true);
+
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === XMLHttpRequest.DONE) {
+                        if (xhr.status === 200) {
+                            console.log(xhr.responseText);
+                        } else {
+                            self.message = 'Error';
+                        }
+                    }
+                };
+                xhr.send();
             }
         });
     </script>
