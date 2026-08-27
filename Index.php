f9ff6082770918174e536d88d6e32358a0250572
edsson878 <30772340+edsson878@users.noreply.github.com>
2026-08-27 10:24:01 -0600

    v1: primer clase

---
+++ Index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="/assets/js/vue/vue.min.js"></script>
+</head>
+<body>
+    <div id="app">
+        <input v-model="message">
+        {{ message }}
+    </div>
+
+    <script>
+        var app =new Vue({
+            el: '#app',
+            data: {
+                message: 'Hello, Vue!'
+            }
+        });
+    </script>
+</body>
+</html>
